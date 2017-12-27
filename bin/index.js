cab425d3ded363614c321265f9f07d904b10f922
Jeremy <jeremyrajan@gmail.com>
2017-12-28 01:11:03 +0530

    lint fixes

--- bin/index.js
+++ bin/index.js
@@ -17,7 +17,7 @@ const initTable = (cols) => {
 
   return {
     table, cols
-  }
+  };
 };
 
 /**
@@ -30,14 +30,14 @@ const initTable = (cols) => {
 const createTableData = (table, cols, list) => {
   // for each item in the result set, push a table row
   // as per the allowed cols defined earlier.
-  list.forEach(element => table.push(cols.map(c => element[c])));
+  list.forEach(element => { table.push(cols.map(c => element[c])) });
   return table.toString();
 };
 
 // process and display.
 inviteList()
   .then((result) => {
-    const {table, cols} = initTable(Object.keys(result[0]));
+    const { table, cols } = initTable(Object.keys(result[0]));
     const tableString = createTableData(table, cols, result);
     console.log(`\nFound ${result.length} results.`);
     console.log(tableString);
